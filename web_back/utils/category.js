fa91677e750dd84ddd6449bfe8681755bb46834d
人间失格 <490008651@qq.com>
2019-12-02 16:17:04 +0800

    封装category

---
+++ web_back/utils/category.js
@@ -0,0 +1,29 @@
+var baseUrl = 'http://localhost:8000/';
+var category = {
+    //获取分类
+    get: function(callback) {
+        $.ajax({
+            type: 'GET',
+            url: baseUrl + 'admin/category_search',
+            success: function(res) {
+                callback(res);
+            }
+        });
+    },
+    //新增分类
+    add: function(name, slug, callback) {
+        //ES6语法，如果对象的键和值同名，则可以写一个s
+        $.post(baseUrl + 'admin/category_add', {
+            name,
+            slug
+        }, function(res) {
+            callback(res)
+
+        });
+    },
+    //删除分类
+    delete: function() {
+
+    },
+    //编辑分类
+}
