37d79b58d383773a2a932bac3c798a8eb1249ce1
人间失格 <490008651@qq.com>
2019-12-02 15:59:33 +0800

    callback回调函数优化代码

--- web_back/utils/user.js
+++ web_back/utils/user.js
@@ -1,38 +1,27 @@
 var baseUrl = 'http://localhost:8000/';
 var user = {
     //登录方法
-    login: function(username, password) {
+    login: function(username, password, callback) {
         $.post(baseUrl + 'admin/login', {
             user_name: username,
             password: password
         }, function(res) {
+            callback(res);
             // console.log(res);
             // alert(res.msg);
-            $('#myMsg').text(res.msg);
-            $('#myModal').modal('show'); // 显示弹框
-            if (res.code === 200) {
-                // 成功，跳转到后台首页
-                location.href = 'index.html';
-            }
-        });
+
+        })
     },
     //退出方法
-    logout: function() {
+    logout: function(callback) {
         $.post(baseUrl + 'admin/logout', function(res) {
-            if (res.code == 200) {
-                location.href = 'login.html';
-            }
+            callback(res);
         })
     },
     //获取用户信息
-    getInfo: function() {
+    getInfo: function(callback) {
         $.get(baseUrl + 'admin/getuser', function(res) {
-            if (res.code == 200) {
-                //更改头像
-                $('.touxiang').prop('src', res.data.user_pic);
-                //更改管理员名字
-                $('#myName').text(res.data.nickname).css('font-style', 'normal');
-            }
+            callback(res);
         })
     }
 }
