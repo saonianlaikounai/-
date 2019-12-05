38d33d8299c77ce7f800612a9b48880892b292bf
人间失格 <490008651@qq.com>
2019-12-05 11:24:09 +0800

    最新资讯渲染完成

--- web_front/utils/article.js
+++ web_front/utils/article.js
@@ -3,5 +3,10 @@ var article = {
         $.get(apis.article_hotpic, function(res) {
             callback(res);
         })
+    },
+    latest: function(callback) {
+        $.get(apis.article_latest, function(res) {
+            callback(res);
+        })
     }
 }
--- web_front/utils/config.js
+++ web_front/utils/config.js
@@ -3,5 +3,6 @@ var baseUrl = 'http://localhost:8000/';
 var apis = {
     //分类相关接口
     category_show: baseUrl + 'category',
-    article_hotpic: baseUrl + 'hotpic',
+    article_hotpic: baseUrl + 'hotpic', // 焦点图接口
+    article_latest: baseUrl + 'latest', // 最新资讯接口
 }
